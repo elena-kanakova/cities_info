94fe7280565f5277f7756e83420fd185fce73daf
e.kanakova <e.kanakova@i-sys.ru>
2019-09-03 17:30:19 +0400

    Статичный вывод первого варианта верстки с выводом пары городов (из объектов) для примера.

--- app/src/CityInfo.test.js
+++ app/src/CityInfo.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './CityInfo';
+import CityInfo from './CityInfo';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<CityInfo />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
